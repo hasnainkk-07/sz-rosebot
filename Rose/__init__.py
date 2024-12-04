ee23d6b714904bdae422f3078c9ffeb9c27570b4
Hasnain khan <halwakk07@gmail.com>
2024-12-04 08:15:51 +0530

    Update __init__.py

--- Rose/__init__.py
+++ Rose/__init__.py
@@ -67,6 +67,6 @@ async def eor(msg: Message, **kwargs):
     return await func(**{k: v for k, v in kwargs.items() if k in spec})
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+#*if __name__ == "__main__":
+   # asyncio.run(main())
  
